341344437159bb0352546dce51543643f08605f3
VadymGol1986 <104563255+VadimGol1986@users.noreply.github.com>
2023-02-24 18:03:24 +0200

    section collection

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict"
+
+//-------Rightside-menu
+document.addEventListener("click", documentActions);
+function documentActions(e) {
+    const targetElement = e.target;
+        if (targetElement.classList.contains ('header__btn--icon')) {
+            document.querySelector('.rightside-menu').classList.remove('rightside-menu--close');
+            }
+        if (targetElement.classList.contains ('rightside-menu__close--icon')) {
+                document.querySelector('.rightside-menu').classList.add('rightside-menu--close');
+            }   
+    }
+//-------Slider
+    if (document.querySelector('.top__slider')) {
+        const swiper = new Swiper('.swiper', {
+            /*direction: 'horizontal',*/
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+              },
+            /*navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },*/
+            slidesPerView: 1,
+            observer: true,
+            observeParents: true,
+            spaceBetween: 50,
+            watchOverflow: true,
+            speed: 800,
+            loopAdditionalSlides: 5,
+            preloadImages: false,
+            parallax: true,
+
+            /*navigation: none, */
+            
+            /*mousewheel: {
+                sensitivity: 1,
+                eventsTarget: ".page__main-slider",
+            }*/
+        });
+    }
